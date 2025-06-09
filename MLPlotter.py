f37098557c65d58ba5e26877b635c2114807d97c
ZachgJack <z.jackson81@vikes.csuohio.edu>
2025-06-09 15:28:55 -0400

    Update MLPlotter.py

--- MLPlotter.py
+++ MLPlotter.py
@@ -1,6 +1,15 @@
 from LSTM_Prototype import train_and_evaluate_lstm
+from K_means_Plotter import run_kmeans_plotter
 
-LaunchLSTM = True
+LaunchLSTM = False
+KMeansPlotter = False
+
+Choice = input("Do you want to launch LSTM  or K-means plotter? ")
+
+if Choice.lower() == "lstm":
+    LaunchLSTM = True
+elif Choice.lower() == "k-means":
+    KMeansPlotter = True
 
 if LaunchLSTM: 
     
@@ -15,3 +24,7 @@ if LaunchLSTM:
         batch_size=32,
         test_size=0.2,
         random_state=42)
+    
+if KMeansPlotter: 
+    csv_path = input(r"Paste the path of the data:")  # or read_csv or whatever format
+    run_kmeans_plotter(csv_path)
